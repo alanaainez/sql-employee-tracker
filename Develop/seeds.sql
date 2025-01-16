38bf17d69ce8269f20bcca278cd66aeafeb35d62
alanaainez <alainez649@gmail.com>
2025-01-15 22:18:36 -0500

    git commit -m "Added department values"

---
+++ Develop/seeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO departments (department_name)
+VALUES ('Management'),
+        ('Sales'),
+        ('Accounting'),
+        ('Human Resources'),
+        ('Reception'),
+        ('Product Oversight'),
+        ('Warehouse');
+
+INSERT INTO employees (last_name, first_name, role_id,)
+VALUES (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        ();
+
+INSERT INTO roles (title, salary, department_id)
+VALUES (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        (),
+        ();
