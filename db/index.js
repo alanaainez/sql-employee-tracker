b3cf8e4929e9a851791193572fb9f2c9c947ae9b
alanaainez <alainez649@gmail.com>
2025-02-14 17:19:58 -0500

    gti commit -m "Added type declarations"

---
+++ db/index.js
@@ -0,0 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const connection_js_1 = require("./connection.js");
+class Db {
+    constructor() { }
+    query(sql_1) {
+        return __awaiter(this, arguments, void 0, function* (sql, args = []) {
+            const client = yield connection_js_1.pool.connect();
+            try {
+                const result = yield client.query(sql, args);
+                return result.rows;
+            }
+            finally {
+                client.release();
+            }
+        });
+    }
+    findAllEmployees() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.query(`
+            SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, 
+            CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM employees
+            LEFT JOIN roles ON employees.role_id = roles.id
+            LEFT JOIN departments ON roles.department_id = departments.id
+            LEFT JOIN employees AS manager ON employees.manager_id = manager.id
+            ORDER BY employees.id;
+        `);
+        });
+    }
+}
+exports.default = Db;
---
+++ dist/prompts.js
@@ -0,0 +1,34 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loadMainPrompts = loadMainPrompts;
+const inquirer_1 = __importDefault(require("inquirer"));
+function loadMainPrompts() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return inquirer_1.default.prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    { name: 'View All Employees', value: 'VIEW_EMPLOYEES' },
+                    { name: 'View All Departments', value: 'VIEW_DEPARTMENTS' },
+                    { name: 'View All Roles', value: 'VIEW_ROLES' },
+                    { name: 'Add Employee', value: 'ADD_EMPLOYEE' },
+                    { name: 'Exit', value: 'EXIT' }
+                ]
+            }
+        ]);
+    });
+}
